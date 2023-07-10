a33cc274d5bc2b8a1f51b88d829e8abc246a89e3
Hrithik Vishwakarma <hritik8008@gmail.com>
2023-07-10 20:10:43 +0530

    edit some env groups

--- Backend/authentication/google.auth.js
+++ Backend/authentication/google.auth.js
@@ -5,44 +5,45 @@ import generator from "generate-password";
 import jwt from "jsonwebtoken";
 import { User_owner } from "../mongodb/models/user.js";
 // var GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
+import * as dotenv from "dotenv";
 
+dotenv.config();
 const GoogleStrategy = Google.Strategy;
 passport.use(
-  new GoogleStrategy(
-    {
-      clientID:
-        "460600247291-0mm4uhnd76o3sk2st8m1184f86qirpos.apps.googleusercontent.com",
-      clientSecret: "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",
-      callbackURL: "http://localhost:8080/auth/google/callback",
-      passReqToCallback: true,
-    },
-    function (request, accessToken, refreshToken, profile, done) {
-      const { given_name, picture, email } = profile;
+	new GoogleStrategy(
+		{
+			clientID: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			callbackURL: "http://localhost:8080/auth/google/callback",
+			passReqToCallback: true,
+		},
+		function (request, accessToken, refreshToken, profile, done) {
+			const { given_name, picture, email } = profile;
 
-      var passwords = generator.generate({
-        length: 18,
-        uppercase: false,
-      });
+			var passwords = generator.generate({
+				length: 18,
+				uppercase: false,
+			});
 
-      bcrypt.hash(passwords, 10, async function (err, hash) {
-        // console.log(hash, err);
-          const checkuser=await User_owner.findOne({email})
-          if(!checkuser){
-            return done(null, profile);
-          }
-        const user = await User_owner.insertMany([
-			{
-				name: profile.displayName,
-				email,
-				password: hash,
-				avatar: profile.picture,
-			},
-		]);
-        return done(null, profile);
-      });
-      console.log({profile})
-    }
-  )
+			bcrypt.hash(passwords, 10, async function (err, hash) {
+				// console.log(hash, err);
+				const checkuser = await User_owner.findOne({ email });
+				if (!checkuser) {
+					return done(null, profile);
+				}
+				const user = await User_owner.insertMany([
+					{
+						name: profile.displayName,
+						email,
+						password: hash,
+						avatar: profile.picture,
+					},
+				]);
+				return done(null, profile);
+			});
+			console.log({ profile });
+		}
+	)
 );
 
 export { passport };
